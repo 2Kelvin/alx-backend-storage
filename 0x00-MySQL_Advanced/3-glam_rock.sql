d2b37482c3ab2215d4fb4d56d02be8bdc4e188e0
2Kelvin <rocketmancodes02@gmail.com>
2024-01-18 12:24:55 +0300

    learn to use coalesce & ifnull

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,7 @@
+-- Old school band
+-- Listing all 'Glam rock' style bands by longetivity
+SELECT band_name,
+    COALESCE(split, 2022) - formed AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;

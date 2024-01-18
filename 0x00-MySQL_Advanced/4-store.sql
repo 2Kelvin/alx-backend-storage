16d57414b2858f9ac0c17d66d47ae7c5a605a2ef
2Kelvin <rocketmancodes02@gmail.com>
2024-01-18 15:54:32 +0300

    use a trigger for my mysql table #2

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,9 +1,8 @@
 -- Buy buy buy
 -- Creating a trigger that decreases item quantity after a new order
 CREATE TRIGGER decreaseItemQuantity
-AFTER INSERT
-ON orders
-FOR EACH ROW
+AFTER
+INSERT ON orders FOR EACH ROW
 UPDATE items
 SET quantity = quantity - NEW.number
-WHERE name = NEW.items_name;
+WHERE name = NEW.item_name;

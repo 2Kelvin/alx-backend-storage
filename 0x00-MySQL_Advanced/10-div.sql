4f1b912103bc080620d2f92824e4e67e4083ed40
2Kelvin <rocketmancodes02@gmail.com>
2024-01-18 19:41:36 +0300

    learn creating mysql functions

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,1 +1,16 @@
 -- Safe divide
+-- Create a division function
+DELIMITER //
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+    RETURNS INT
+    DETERMINISTIC
+    BEGIN
+        IF b == 0 THEN
+            RETURN 0;
+        ENDIF;
+
+        RETURN a / b;
+    END //
+
+DELIMITER ;

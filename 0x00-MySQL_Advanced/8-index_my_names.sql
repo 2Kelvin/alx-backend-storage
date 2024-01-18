0892ece87804c0f09efec0e1945e00fe37aa2d75
2Kelvin <rocketmancodes02@gmail.com>
2024-01-18 19:20:36 +0300

    learn about indexes

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,3 +1,3 @@
 -- Optimize simple search
 -- Creating an index
-CREATE INDEX idx_name_first on names(name(1));
+CREATE INDEX idx_name_first ON names(name(1));
--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,3 +1,3 @@
 -- Optimize search and score
 -- Creating an index
-CREATE INDEX idx_name_first on names(name(1), score);
+CREATE INDEX idx_name_first_score ON names(name(1), score);

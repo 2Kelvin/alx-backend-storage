4f9c33321f9d86494bec136d7eec81fa6374d754
2Kelvin <rocketmancodes02@gmail.com>
2024-01-18 20:02:24 +0300

    fix task 10 errs #5

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -2,17 +2,10 @@
 -- Create a division function
 DELIMITER //
 
-CREATE FUNCTION SafeDiv(a INT, b INT)
-    RETURNS FLOAT
-    DETERMINISTIC
-    BEGIN
-        DECLARE answer FLOAT DEFAULT 0;
-
-        IF b != 0 THEN
-            SET answer = a / b;
-        ENDIF;
-
-        RETURN answer;
-    END //
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
+BEGIN
+    RETURN (IF (b = 0, 0, a / b));
+END //
 
 DELIMITER ;

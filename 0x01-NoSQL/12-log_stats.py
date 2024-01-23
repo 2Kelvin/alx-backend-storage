981bf5233898a7adf26c063c3c15affc796b5b5b
2Kelvin <rocketmancodes02@gmail.com>
2024-01-24 01:24:10 +0300

    task 12 fix #10

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,21 +3,30 @@
 from pymongo import MongoClient
 
 
-def nginxLogs(collection):
+def nginxLogs():
     '''provides nginx log stats stored in mongodb'''
-    print(f'{collection.count_documents({})} logs')
+    mongoClient = MongoClient("mongodb://localhost:27017")
+    collection = mongoClient.logs.nginx
+
+    allLogs = collection.count_documents({})
+    getLogs = collection.count_documents({'method': 'GET'})
+    postLogs = collection.count_documents({'method': 'POST'})
+    putLogs = collection.count_documents({'method': 'PUT'})
+    patchLogs = collection.count_documents({'method': 'PATCH'})
+    deleteLogs = collection.count_documents({'method': 'DELETE'})
+    pathLogs = collection.count_documents({'method': 'GET', 'path': '/status'})
+
+    print(f'{allLogs} logs')
     print('Methods:')
 
-    httpMethods = ['GET', 'POST', 'PUT', 'PATCH' 'DELETE']
+    print(f'\tmethod GET: {getLogs}')
+    print(f'\tmethod POST: {postLogs}')
+    print(f'\tmethod PUT: {putLogs}')
+    print(f'\tmethod PATCH: {patchLogs}')
+    print(f'\tmethod DELETE: {deleteLogs}')
 
-    for httpMethod in httpMethods:
-        methodCount = len(list(collection.find({"method": httpMethod})))
-        print(f'\tmethod {httpMethod}: {methodCount}')
-
-    getCount = len(list(collection.find({"method": "GET", "path": "/status"})))
-    print(f'{getCount} status check')
+    print(f'{pathLogs} status check')
 
 
 if __name__ == '__main__':
-    mongoClient = MongoClient("mongodb://localhost:27017")
-    nginxLogs(mongoClient.logs.nginx)
+    nginxLogs()

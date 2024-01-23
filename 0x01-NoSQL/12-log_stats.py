26260181c62d0dbc078b27768bf9111b004b4715
2Kelvin <rocketmancodes02@gmail.com>
2024-01-24 00:55:00 +0300

    add tasks 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 '''Log stats'''
+from pymongo import MongoClient
 
 
-def nginxLogs():
+def nginxLogs(collection):
     '''provides nginx log stats stored in mongodb'''
+    print(f'{collection.count()} logs')
+
+    print('Methods:')
+
+    httpMethods = ['GET', 'POST', 'DELETE', 'PATCH', 'PUT']
+
+    for httpMethod in httpMethods:
+        methodCount = collection.count_documents({"method": httpMethod})
+        print(f'\tmethod {httpMethod}: {methodCount}')
+
+    getCount = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f'{getCount} status check')
+
+
+if __name__ == '__main__':
+    collection = MongoClient("mongodb://localhost:27017/").logs.nginx
+    nginxLogs(collection)

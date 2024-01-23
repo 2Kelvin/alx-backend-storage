91d9a5d230229cb410de78ab5db16124d4ea0689
2Kelvin <rocketmancodes02@gmail.com>
2024-01-24 00:32:10 +0300

    add new document & return its id

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 '''List all documents in the mongodb collection'''
-from pymongo import MongoClient
 
 
 def list_all(mongo_collection):
     '''listing all documents in the passed collection'''
-    client = MongoClient('localhost', 27017)
-    database = client.mongo_collection
+    return [eachDocument for eachDocument in mongo_collection.find()]
--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -4,3 +4,5 @@
 
 def insert_school(mongo_collection, **kwargs):
     '''inserts a new document'''
+    newDocument = mongo_collection.insert_one(kwargs)
+    return newDocument.inserted_id

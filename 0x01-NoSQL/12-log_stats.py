f6b062bbaf1e21f907ae806de7939cfe31feb1a3
2Kelvin <rocketmancodes02@gmail.com>
2024-01-24 01:04:17 +0300

    task 12 fix

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python3
 '''Log stats'''
-import pymongo
 from pymongo import MongoClient
 
 
-def nginxLogs(mongo_collection):
+def nginxLogs(collection):
     '''provides nginx log stats stored in mongodb'''
-    print(f'{mongo_collection.estimated_document_count()} logs')
-
+    print(f'{collection.count_documents()} logs')
     print('Methods:')
 
     httpMethods = ['GET', 'POST', 'DELETE', 'PATCH', 'PUT']
 
     for httpMethod in httpMethods:
-        methodCount = mongo_collection.count_documents({"method": httpMethod})
+        methodCount = collection.count_documents({"method": httpMethod})
         print(f'\tmethod {httpMethod}: {methodCount}')
 
-    getCount = mongo_collection.count_documents({"method": "GET", "path": "/status"})
+    getCount = collection.count_documents({"method": "GET", "path": "/status"})
     print(f'{getCount} status check')
 
 
 if __name__ == '__main__':
-    mongo_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
-    nginxLogs(mongo_collection)
+    collection = MongoClient("mongodb://localhost:27017")
+    nginxLogs(collection.logs.nginx)

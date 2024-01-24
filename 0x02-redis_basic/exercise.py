820d01ae0a24b3a370049d275470a2f8781504c9
2Kelvin <rocketmancodes02@gmail.com>
2024-01-24 16:37:00 +0300

    recovering original data type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 '''Learning and using Redis'''
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -19,3 +19,28 @@ class Cache:
         randomKey: str = str(uuid.uuid4())
         self._redis.set(randomKey, data)
         return randomKey
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        '''custom get() method'''
+        value = self._redis.get(key)
+        if not value:
+            return None
+
+        if fn:
+            value = fn(value)
+
+        return value
+
+    def get_str(self, key: str) -> str:
+        '''convert redis data to regular string'''
+        return self._redis.get(key).decode('utf-8')
+
+    def get_int(self, key: str) -> int:
+        '''convert redis data to regular int'''
+        convertedInt = self._redis.get(key)
+        try:
+            convertedInt = int(convertedInt.decode('utf-8'))
+        except Exception:
+            convertedInt = 0
+        return convertedInt

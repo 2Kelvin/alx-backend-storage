e846b45f2cdc8da238e398e939cf9c97477b4c01
2Kelvin <rocketmancodes02@gmail.com>
2024-01-24 16:02:01 +0300

    rename param to fix the error

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -13,8 +13,8 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, dataParam: Union[str, bytes, int, float]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         '''stores data in Redis db using the randomly generated key'''
         randomKey: str = str(uuid.uuid4())
-        self._redis.set(randomKey, dataParam)
+        self._redis.set(randomKey, data)
         return randomKey
